35ef422982b9a8615f33ae18a6e106b1ca7463bd
huadong100 <huadong100@dingtalk.com>
2019-02-20 12:31:08 +0800

    上传 webpack.config.js配置文件

---
+++ build/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'none',
+	entry: {
+		app: path.join(__dirname, '../client/app.js')
+	},
+	output: {
+		filename: '[name].[hash].js',
+		path: path.join(__dirname, '../dist/'),
+		publicPath: ''
+		// publicPath: '/public'
+	},
+	module: {
+		rules: [
+			{
+				test: /.(jsx|js)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader'
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlPlugin()
+	]
+}
