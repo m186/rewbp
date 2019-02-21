044b516a93f15c9379d6a3505e91af020e658473
huadong100 <huadong100@dingtalk.com>
2019-02-21 19:42:32 +0800

    开发时的服务端渲染设置

--- server/index.js
+++ server/index.js
@@ -1,30 +1,23 @@
 const express = require('express')
-const Koa = require('koa')
-const koaStatic = require('koa-static')
 const fs = require('fs')
 const path = require('path')
 const ReactSSR = require('react-dom/server')
-const serverEntry = require('../dist/serverEntry').default
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
 
-/**
- * express 使用
- */
 const app = express()
-app.use('/public', express.static(path.join(__dirname, '../dist')))
-app.get('*', (req, res) => {
-	const htmlString = ReactSSR.renderToString(serverEntry)
-	res.send(template.replace('<!-- app -->', htmlString))
-})
-	
-/**
- * koa 使用
- */
-// const app = new Koa()
-// app.use(async (ctx, next) => {
-// 	const htmlString = ReactSSR.renderToString(serverEntry)
-// 	ctx.body = template.replace('<app></app>', htmlString)
-// })
+const isDev = process.env.NODE_ENV === 'development'
+
+if (!isDev) {
+	const serverEntry = require('../dist/serverEntry').default
+	const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+	app.use('/public', express.static(path.join(__dirname, '../dist')))
+	app.get('*', (req, res) => {
+		const htmlString = ReactSSR.renderToString(serverEntry)
+		res.send(template.replace('<!-- app -->', htmlString))
+	})
+} else {
+	const devStatic = require('./util/dev-static')
+	devStatic(app)
+}
 
 app.listen(4000, () => {
 	console.log('服务端口：http://localhost:4000')
---
+++ server/util/dev-static.js
@@ -0,0 +1,52 @@
+const axios = require('axios')
+const webpack = require('webpack')
+const path = require('path')
+const proxy = require('http-proxy-middleware')
+const MemoryFs = require('memory-fs')
+const ReactDomServer = require('react-dom/server')
+
+const serverConfig = require('../../build/webpack.config.server')
+
+const getTemplate = () => {
+	return new Promise((resolve, reject) => {
+		axios.get('http://localhost:3333/public/index.html')
+			.then(res => {
+				resolve(res.data)
+			})
+			.catch(reject())
+	})
+}
+
+const Module = module.constructor
+const mfs = new MemoryFs
+const serverCompiler = webpack(serverConfig)
+serverCompiler.outputFileSystem = mfs
+
+let serverBundle
+serverCompiler.watch({}, (err, stats) => {
+	if (err) throw err
+	stats = stats.toJson()
+	stats.errors.forEach(err => console.error(err))
+	stats.warnings.forEach(warn => console.warn(warn))
+	const bundlePath = path.join(
+		serverConfig.output.filename,
+		serverConfig.output.path
+	)
+	const bundle = mfs.readFileSync(bundlePath, 'utf-8')
+	const m = new Module()
+	m._compile(bundle, 'server-entry.js')
+	serverBundle = m.exports.default
+})
+
+module.exports = (app) => {
+	app.use('/public', proxy({
+		target: 'http://localhost:3333'
+	}))
+
+	app.get('*', (req, res) => {
+		getTemplate().then(tpl => {
+			const content = ReactDomServer.renderToString(serverBundle)
+			res.send(tpl.replace('<!-- app -->', content))
+		})
+	})
+}
