e87723cbc1e3557636cc32585439bf282474a27e
huadong100 <huadong100@dingtalk.com>
2019-03-02 11:28:12 +0800

    新建项目目录

--- client/app.js
+++ client/app.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader' // 包裹需要热更新的内容
-import App from './App.jsx'
+import App from './views/App'
 
 const root = document.getElementById('root')
 const render = Component => {
   ReactDOM.hydrate(
     <AppContainer>
-    <Component/>
-  </AppContainer>, root)
+      <Component/>
+    </AppContainer>, 
+    root
+  )
 }
 
 render(App)
 
 if (module.hot) {
   // 当热更新后，重新加载 app 文件并 “重新渲染”
-  module
-    .hot
-    .accept('./App.jsx', () => {
-      const NextApp = require('./App.jsx').default
+  module.hot.accept('./views/App', () => {
+      const NextApp = require('./views/App').default
       render(NextApp)
     })
 }
